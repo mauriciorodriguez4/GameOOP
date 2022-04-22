c42df15c04c17e45ba7d471ef8641d88e36e949f
mauriciorodriguez4 <98993011+mauriciorodriguez4@users.noreply.github.com>
2022-04-22 13:59:24 -0600

    Update Magician.java

--- Magician.java
+++ Magician.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gameoop;
 
 /**
@@ -10,19 +5,18 @@ package gameoop;
  * @author amand
  */
 public class Magician extends GameUtil {
-    public Magician()
-    {
+
+    public Magician() {
         name = "Mago";
         life = 50;
-        attack =25;
-        defense =15;
-        hr=0.75;
+        attack = 25;
+        defense = 15;
+        hr = 0.75;
     }
 
     @Override
-    public void metodo2() {
-        System.out.println(this.name+" uso su varita magica y logro desviaar el ataque!!"); //To change body of generated methods, choose Tools | Templates.
+    public void esquivarAtaque() {
+        System.out.println(this.name + " uso su varita magica y logro desviaar el ataque!!"); 
     }
-    
-    
+
 }
